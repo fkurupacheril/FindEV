3ffa7012c36f5023677be91619e1a7cf917e5ce1
Francis <francis.kurupacheril@gmail.com>
2013-12-01 14:19:42 -0800

    Initial Attempt
    At writing a iOS EV calculator

--- FindEV/ViewController.h
+++ FindEV/ViewController.h
@@ -8,6 +8,59 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+#define INITIAL_COST_SAVINGS                            0
+#define INITIAL_VALUE_SETTING                           0
+#define AVERAGE_BREAKEVEN_YEARS                     10.69
+#define TOTAL_CURRENT_SAVINGS                           0
+#define AVERAGE_CHARGE_PER_MILE_COST                 0.05
+#define LENGTH_OF_DISPLAY_STRINGS                     500
+#define INITIAL_GAS_PER_MONTH                          10
+#define INITIAL_MILES_PER_DAY                           1
+#define INITIAL_ELEC_PER_MONTH                         20
+#define AVERAGE_DAYS_PER_MONTH                      30.14
+#define LEAST_COST_SAVINGS_NEEDED_FOR_VOLT           7000
+#define LEAST_COST_SAVINGS_NEEDED_FOR_FOCUS_ELECTRIC 8000
+#define LEAST_COST_SAVINGS_NEEDED_FOR_LEAF          12000
+#define LEAST_COST_SAVINGS_NEEDED_FOR_CODA          20000
+#define LEAST_COST_SAVINGS_NEEDED_FOR_TESLA         25000
+#define LEAST_COST_SAVINGS                           7000
+
+@interface ViewController : UIViewController {
+  	UILabel *displayGas;
+	UILabel *displayMiles;
+	UILabel *displayElec;
+	UILabel *displayFinalResult;
+	float cost_savings_per_month;
+	float average_breakeven_years;
+	float average_charge_per_mile_cost;
+	float total_cost_to_charge_EV_per_month;
+	float total_current_savings;
+	float total_electricity_bill_per_month;
+	float gasperMonth;
+	float elecperMonth;
+	float milesperDay;
+	NSMutableString  *displayResultString;
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *displayFinalResult;
+@property (nonatomic, retain) IBOutlet UILabel *displayGas;
+@property (nonatomic, retain) IBOutlet UILabel *displayElec;
+@property (nonatomic, retain) IBOutlet UILabel *displayMiles;
+@property (nonatomic, retain) NSMutableString  *displayResultString;
+
+-(void) processGasExpensesPerMonth: (float) gasExpenses;
+-(void) processElectricityExpensesPerMonth: (float) electricExpenses;
+-(void) processTotalMilesDrivenDaily: (float) totalMiles;
+-(void) processAppropriateEV;
+-(void) processGasExpensesLabel:(float) gasExpenses;
+-(void) processElectricityExpensesLabel:(float) electricExpenses;
+-(void) processTotalMilesDailyLabel:(float) totalMiles;
+
+// Each Action corresponds to each bar and the final calculate button
+
+-(IBAction) gasBar: (UISlider *) sender;
+-(IBAction) electricityBar: (UISlider *) sender;
+-(IBAction) milesBar: (UISlider *) sender;
+-(IBAction) clickFindEV: (id) sender;
 
 @end
--- FindEV/ViewController.m
+++ FindEV/ViewController.m
@@ -9,6 +9,148 @@
 #import "ViewController.h"
 
 @implementation ViewController
+@synthesize displayGas, displayMiles, displayElec, displayFinalResult, displayResultString;
+
+
+/*
+ * Display functions for each slider movement
+ */
+
+-(void) processGasExpensesLabel:(float) gasExpenses
+{
+	NSString* myNewString = [NSString stringWithFormat:@"$%.2f", gasExpenses];
+	[displayGas setText: myNewString];	
+}
+
+-(void) processElectricityExpensesLabel:(float) electricExpenses 
+{
+	NSString* myNewString = [NSString stringWithFormat:@"$%.2f", electricExpenses];
+	[displayElec setText: myNewString];	
+}
+
+-(void) processTotalMilesDailyLabel:(float) totalMiles
+{
+	NSString* myNewString = [NSString stringWithFormat:@"%.2f", totalMiles];
+    [displayMiles setText: myNewString];	
+}
+
+/*
+ * Assign values when the sliders are moved
+ */
+
+-(void) processGasExpensesPerMonth:(float) gasExpenses
+{
+	gasperMonth = gasExpenses ;
+}
+
+-(void) processElectricityExpensesPerMonth:(float) electricExpenses 
+{
+	elecperMonth = electricExpenses ;
+}
+
+-(void) processTotalMilesDrivenDaily:(float) totalMiles
+{
+	milesperDay = totalMiles ;
+}
+
+/*
+ * The method that processes the button. 
+ */
+
+-(void) processAppropriateEV 
+{
+	total_current_savings = INITIAL_VALUE_SETTING ; 
+	total_cost_to_charge_EV_per_month = AVERAGE_DAYS_PER_MONTH * milesperDay * average_charge_per_mile_cost;
+	cost_savings_per_month = gasperMonth - total_cost_to_charge_EV_per_month;
+	total_current_savings = cost_savings_per_month * average_breakeven_years;
+	total_electricity_bill_per_month = total_cost_to_charge_EV_per_month + elecperMonth ;
+	
+	/*
+	 * Display the correct messages depending on the ranges calculated above. May have to 
+     * add more EV's and also correspondingly adjust the ranges.
+	 */
+	
+	if (total_current_savings <= 0) {
+        [displayResultString setString:[NSString stringWithFormat:@"---------------Invalid entries----------------and all the three slider values have to make sense"]];
+        [displayFinalResult setText:displayResultString];
+    }
+    
+    else if ((total_current_savings) > 0 && (total_current_savings < LEAST_COST_SAVINGS)) {
+        [displayResultString setString:[NSString stringWithFormat:@"Over 10 years, you save only $%.2f and No EV is affordable with such cost savings", total_current_savings]];
+        [displayFinalResult setText: displayResultString];
+	} 
+    
+    else if ((total_current_savings) > LEAST_COST_SAVINGS_NEEDED_FOR_TESLA) {
+        [displayResultString setString:[NSString stringWithFormat:@"Over 10 years, you save $%.2f and Your Affordable EV = Tesla Model S and New monthly Electric bill = $%.2f",
+           total_current_savings, total_electricity_bill_per_month]];
+        [displayFinalResult setText: displayResultString];
+
+    } 
+    
+    else if ((total_current_savings >= LEAST_COST_SAVINGS_NEEDED_FOR_VOLT) && (total_current_savings < LEAST_COST_SAVINGS_NEEDED_FOR_FOCUS_ELECTRIC )) {
+        [displayResultString setString:[NSString stringWithFormat:@"Over 10 years, you save $%.2f and Your Affordable EV = Chevrolet Volt and New monthly Electric bill = $%.2f",
+           total_current_savings, total_electricity_bill_per_month]];
+        [displayFinalResult setText: displayResultString];
+    }
+          
+    else if ((total_current_savings >= LEAST_COST_SAVINGS_NEEDED_FOR_FOCUS_ELECTRIC) && (total_current_savings < LEAST_COST_SAVINGS_NEEDED_FOR_LEAF )) {
+        [displayResultString setString:[NSString stringWithFormat:@"Over 10 years, you save $%.2f and Your Affordable EV = Ford Focus Electric and New monthly Electric bill = $%.2f",
+           total_current_savings, total_electricity_bill_per_month]];
+        [displayFinalResult setText: displayResultString];
+    }
+    
+    else if ((total_current_savings >= LEAST_COST_SAVINGS_NEEDED_FOR_LEAF) && (total_current_savings < LEAST_COST_SAVINGS_NEEDED_FOR_CODA )) {
+        [displayResultString setString:[NSString stringWithFormat:@"Over 10 years, you save $%.2f and Your Affordable EV = Nissan Leaf and New monthly Electric bill = $%.2f",
+          total_current_savings, total_electricity_bill_per_month]];
+        [displayFinalResult setText: displayResultString];
+    }
+         
+    else if ((total_current_savings >= LEAST_COST_SAVINGS_NEEDED_FOR_CODA) && (total_current_savings < LEAST_COST_SAVINGS_NEEDED_FOR_TESLA )) {
+        [displayResultString setString:[NSString stringWithFormat:@"Over 10 years, you save $%.2f and Your Affordable EV = CODA Coda and New monthly Electric bill = $%.2f",
+          total_current_savings, total_electricity_bill_per_month]];
+        [displayFinalResult setText: displayResultString];
+
+    } 
+	
+	total_current_savings = INITIAL_VALUE_SETTING ; 
+	total_cost_to_charge_EV_per_month = INITIAL_VALUE_SETTING;
+	cost_savings_per_month = INITIAL_VALUE_SETTING;
+	total_electricity_bill_per_month = INITIAL_VALUE_SETTING;
+}
+
+
+-(IBAction) gasBar: (UISlider *) sender
+{
+	UISlider *slider = (UISlider*)sender;
+    float sliderValue = [slider value];
+	[self processGasExpensesPerMonth:sliderValue];
+	[self processGasExpensesLabel:sliderValue];
+}
+
+-(IBAction) electricityBar: (UISlider *) sender
+{
+	UISlider *slider = (UISlider*)sender;
+    float sliderValue = [slider value];
+	[self processElectricityExpensesPerMonth:sliderValue];
+	[self processElectricityExpensesLabel:sliderValue];
+}
+
+-(IBAction) milesBar: (UISlider *) sender
+{
+	UISlider *slider = (UISlider*)sender;
+    float sliderValue = [slider value];
+	[self processTotalMilesDrivenDaily:sliderValue];
+	[self processTotalMilesDailyLabel:sliderValue];
+}
+
+-(IBAction) clickFindEV: (id) sender
+{
+	[self processAppropriateEV];
+}
+
+/*===========================================================
+ * Code Added by Xcode. To be implemented.
+ */
 
 - (void)didReceiveMemoryWarning
 {
@@ -20,13 +162,37 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+    
+    /*
+     * Set all the initial values. 
+     */
+    
+    cost_savings_per_month = INITIAL_COST_SAVINGS;
+	average_breakeven_years = AVERAGE_BREAKEVEN_YEARS; // Ask Francis Kurupacheril if you wanna know how this was calculated :)
+	total_current_savings = TOTAL_CURRENT_SAVINGS;
+	average_charge_per_mile_cost = AVERAGE_CHARGE_PER_MILE_COST; // It costs around 3-5 cents to charge an EV for it to drive a mile
+	self.displayFinalResult = [NSMutableString stringWithCapacity: LENGTH_OF_DISPLAY_STRINGS];
+    gasperMonth = INITIAL_GAS_PER_MONTH;
+    milesperDay = INITIAL_MILES_PER_DAY;
+    elecperMonth = INITIAL_ELEC_PER_MONTH;
+    
+    /*
+     * Display the initial slider values.
+     */
+    
+    [self processGasExpensesLabel:gasperMonth];
+    [self processElectricityExpensesLabel:elecperMonth];
+    [self processTotalMilesDailyLabel:milesperDay];
+    
+    
+   // [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
+    
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
@@ -49,6 +215,12 @@
 - (void)viewDidDisappear:(BOOL)animated
 {
 	[super viewDidDisappear:animated];
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+
+    return YES;
+
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
